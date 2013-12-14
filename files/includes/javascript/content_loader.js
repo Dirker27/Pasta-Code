61064bce7495093f468e02a6523dbee98990b6b3
Dirk <hortensd@gmail.com>
2013-12-14 18:21:27 -0500

    static/dynamic content

---
+++ files/includes/javascript/content_loader.js
@@ -0,0 +1,5 @@
+function load_content() {
+	document.getElementById('page_header').innerHTML = 'Dynamic Header';
+	document.getElementById('page_body').innerHTML = 'Dynamic Body';
+	document.getElementById('page_footer').innerHTML = 'Dynamic Footer';
+}
