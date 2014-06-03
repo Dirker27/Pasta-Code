99cc93175e3003b00e61354a3275fbb718e425b2
Dirk <hortensd@gmail.com>
2014-06-02 22:20:55 -0400

    prototype added

--- files/js/content_loader.js
+++ files/js/content_loader.js
@@ -2,4 +2,5 @@ function load_content() {
 	//document.getElementById('page_header').innerHTML = 'Dynamic Header';
 	//document.getElementById('page_body').innerHTML = 'Dynamic Body';
 	//document.getElementById('page_footer').innerHTML = 'Dynamic Footer';
+	//$('target').innerHTML = "SUCK IT BITCH";
 }
