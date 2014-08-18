25baaa051b61c3e0e17775ee8493be4272aef773
Dirk <hortensd@gmail.com>
2014-08-18 11:26:17 -0700

    tab fading

--- files/js/content_loader.js
+++ files/js/content_loader.js
@@ -3,9 +3,31 @@ function page_init() {
 }
 
 function load_body(i) {
-	$('page_body').innerHTML = $(i).innerHTML;
+	$('fade_layer').style.opacity = $('content_layer').style.opacity;
+	$('fade_layer').innerHTML = $('content_layer').innerHTML;
+
+	$('content_layer').style.opacity = 0;
+	$('content_layer').innerHTML = $(i).innerHTML;
 }
 
 function load_tab(tab) {
 	load_body(tab + '_content');
+
+	var op = 0;
+	$('fade_layer').style.display = 'block';
+	var timer = setInterval(function() {
+		if (op > 0.95) {
+			$('content_layer').style.opacity = 1;
+			$('fade_layer').style.display = 'none';
+			//$('content_layer').style.filter = 'alpha(opacity=100)';
+			clearInterval(timer);
+		}
+		$('content_layer').style.opacity = op;
+		$('fade_layer').style.opacity = 1 - op;
+		//$('content_layer').style.filter = 'alpha(opacity=' + op * 100 + ")";
+		op += 0.05
+		//op -= op * 0.1;
+	}, 10);
+
+	$('page_body').style.height = $('content_layer').style.height;
 }
