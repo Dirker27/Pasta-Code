d9d4b53d9249bb4144b38aaddb861c95b8f33235
Dirk Hortensius <hortensd@amazon.com>
2015-07-27 21:41:50 -0700

    Updated Content Tabs. All now JQuery-injected

--- files/js/page_initializer.js
+++ files/js/page_initializer.js
@@ -3,7 +3,15 @@ var $j = jQuery.noConflict();
 
 // Page start-up
 function page_init() {
-	load_tab('home');
 	$j("#page_header").load("html/header.html");
     $j("#page_footer").load("html/footer.html");
+    
+    $j("#home_content").load("html/home.html", function() {
+            load_tab('home');
+        });
+    $j("#music_content").load("html/music.html");
+    $j("#gaming_content").load("html/gaming.html");
+    $j("#about_content").load("html/about.html");
+    
+    load_tab('home');
 }
