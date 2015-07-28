2ec627471366e49d5c4fcfa97424407b2a57fdb3
Dirk Hortensius <hortensd@amazon.com>
2015-07-28 13:23:30 -0700

    Deprecating Footer for now

--- files/js/page_initializer.js
+++ files/js/page_initializer.js
@@ -4,7 +4,7 @@ var $j = jQuery.noConflict();
 // Page start-up
 function page_init() {
 	$j("#page_header").load("html/header.html");
-    $j("#page_footer").load("html/footer.html");
+    //$j("#page_footer").load("html/footer.html");
     
     $j("#home_content").load("html/home.html", function() {
             load_tab('home');
@@ -12,6 +12,4 @@ function page_init() {
     $j("#music_content").load("html/music.html");
     $j("#gaming_content").load("html/gaming.html");
     $j("#about_content").load("html/about.html");
-    
-    load_tab('home');
 }
