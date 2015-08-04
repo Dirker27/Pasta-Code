1acd383546fb77c8adac7d61c30af52e11e8b80b
Dirk Hortensius <hortensd@amazon.com>
2015-08-04 14:56:06 -0700

    Header Image and Quote swapping for tabs

--- files/js/content_loader.js
+++ files/js/content_loader.js
@@ -6,8 +6,15 @@ function load_body(i) {
 	$('content_layer').innerHTML = $(i).innerHTML;
 }
 
+function load_header(i) {
+    $('page_header').style.backgroundImage = 'url("' + $g_assetMap['header_background'][i] + '")';
+    $('quote_content').innerHTML = $g_assetMap['header_quote'][i][0];
+    $('quote_source').innerHTML = $g_assetMap['header_quote'][i][1];
+}
+
 function load_tab(tab) {
 	load_body(tab + '_content');
+    load_header(tab);
 
 	var content = $('content_layer');
 	var fader = $('fade_layer');
--- files/js/page_initializer.js
+++ files/js/page_initializer.js
@@ -1,15 +1,45 @@
 // Tell JQuery to play nice with Prototype
 var $j = jQuery.noConflict();
 
+//- Define Asset Map -------------------------------------=
+//
+var $g_assetMap = {};
+// Header Background
+$g_assetMap['header_background'] = {};
+$g_assetMap['header_background']['home'] = 'img/background/in_flames.jpg';
+$g_assetMap['header_background']['music'] = 'img/background/periphery.jpg';
+$g_assetMap['header_background']['gaming'] = 'img/background/kmfdm.jpg';
+$g_assetMap['header_background']['about'] = 'img/badge/silhouette.jpg';
+// Header Quote
+$g_assetMap['header_quote'] = {};
+$g_assetMap['header_quote']['home'] = new Array();
+$g_assetMap['header_quote']['home'][0] = "Don't Panic.";
+$g_assetMap['header_quote']['home'][1] = "- Douglas Adams";
+$g_assetMap['header_quote']['music'] = new Array();
+$g_assetMap['header_quote']['music'][0] = "I did a thing!";
+$g_assetMap['header_quote']['music'][1] = "- God";
+$g_assetMap['header_quote']['gaming'] = new Array();
+$g_assetMap['header_quote']['gaming'][0] = "I'm too drunk to taste this chicken.";
+$g_assetMap['header_quote']['gaming'][1] = "- Colonel Sanders";
+$g_assetMap['header_quote']['about'] = new Array();
+$g_assetMap['header_quote']['about'][0] = "Why does this bacon taste like people?";
+$g_assetMap['header_quote']['about'][1] = "- Dirk Hortensius";
+// Page Background
+$g_assetMap['page_background'] = {};
+$g_assetMap['header_background']['home'] = 'img/background/10.jpg';
+$g_assetMap['header_background']['music'] = 'img/background/periphery.jpg';
+$g_assetMap['header_background']['gaming'] = 'img/background/kmfdm.jpg';
+$g_assetMap['header_background']['about'] = 'img/badge/silhouette.jpg';
+
 // Page start-up
 function page_init() {
-	$j("#page_header").load("html/header.html");
+	$j('#page_header').load('html/header.html');
     //$j("#page_footer").load("html/footer.html");
     
-    $j("#home_content").load("html/home.html", function() {
+    $j('#home_content').load('html/home.html', function() {
             load_tab('home');
         });
-    $j("#music_content").load("html/music.html");
-    $j("#gaming_content").load("html/gaming.html");
-    $j("#about_content").load("html/about.html");
+    $j('#music_content').load('html/music.html');
+    $j('#gaming_content').load('html/gaming.html');
+    $j('#about_content').load('html/about.html');
 }
